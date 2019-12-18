7cf638630d5fb815e126f2d4ee2964ff616273c6
Jason Adam <jason@myndshft.com>
2019-12-18 10:47:24 -0700

    initial model commit

---
+++ build/CMakeFiles/pytorch-cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pytorch-cpp.dir/dcgan.cpp.o"
+  "pytorch-cpp"
+  "pytorch-cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pytorch-cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
